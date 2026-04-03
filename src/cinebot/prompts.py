ccdbed3e4d5f81cb85ea024389f1829efbca46a2
Karthik-005 <pvkarthik173@gmail.com>
2026-04-04 00:06:41 +0530

    add prompt template for the llm

---
+++ src/cinebot/prompts.py
@@ -0,0 +1,20 @@
+from langchain_core.prompts import ChatPromptTemplate, PromptTemplate, MessagesPlaceholder
+
+SYSTEM_MESSAGE = "You are a movie expert and your job is to suggest movies based on the context. If the context is not sufficient then do not hallucinate, just apologize for not having enough information. Do not mention anything related to the context section in your response. You might get queries related to the previous queries, use only your previous responses in chat history to answer these queries" 
+
+HUMAN_MESSAGE = """Answer the given query using only the following context,
+
+Context:
+{context}
+
+Query: {query}
+"""
+
+CHAT_TEMPLATE = ChatPromptTemplate.from_messages([
+	("system", SYSTEM_MESSAGE),
+	MessagesPlaceholder(variable_name="chat_history"),
+	("human", HUMAN_MESSAGE)
+])
+
+DOC_TEMPLATE = PromptTemplate.from_template("""Title: {Title} \n Release year: {Year_of_release} \n
+                                                   Directors: {Directors} \nCast: {Cast} \nRating: {Rating} \nGenres: {genres}""")
