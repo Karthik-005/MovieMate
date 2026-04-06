e99a271ed1b15315ba0f5dec680d749122ccd309
Karthik-005 <pvkarthik173@gmail.com>
2026-04-06 20:46:01 +0530

    remove the API configuration code

--- src/cinebot/complete_setup.py
+++ src/cinebot/complete_setup.py
@@ -1,25 +1,6 @@
 from cinebot import ingestion, database
-from cinebot.config import settings
-from dotenv import load_dotenv, set_key, dotenv_values
-import os
-
-def complete_setup():
-	# API keys and tokens
-	env_path = settings.ROOT_DIR / '.env'
-	
-	if not load_dotenv():
-		print("Creating .env file....")
-		env_path.touch()
-	
-	config = dotenv_values(env_path)
-	if "HF_TOKEN" not in config:
-		hf_token = input("Enter your Hugging face token: ").strip()
-		set_key(env_path, "HF_TOKEN", hf_token)
-		
-	if "TMDB_READ_ACCESS" not in config:
-		tmdb_read_access = input("Enter your TMDB read access key: ")
-		set_key(env_path, "TMDB_READ_ACCESS", tmdb_read_access).strip()
-	
+from cinebot.config import settings 
+def complete_setup(read_access, hf_token):
 	# data pipeline
 	base_url = settings.BASE_URL
 	raw_data_path = settings.RAW_DATA_PATH
